7f894de24bc913034e3e8944da346ccd770bc3d9
Pär Thernström <par@earthpeople.se>
2016-12-16 11:43:38 +0100

    add GA to popup. TRACK EVERYTHING!!!

--- src/main.js
+++ src/main.js
@@ -26,12 +26,27 @@ console.log("this is popup.js");
 
   }
 
+  function initGA() {
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-44872713-18']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = 'https://ssl.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
+  }
+
   function init() {
 
     initVue();
+    initGA();
 
     console.log("init data", data);
-
+    
     // ...query for the active tab...
     chrome.tabs.query({
         active: true,
